9fcf7f04968f2d9ef007f4c6f370c2990642fc23
“Kiné <>
2026-05-04 22:40:26 +0000

    Correction de la structure des dossiers et config Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#1e293b", 
+        primary: "#1e293b",
         secondary: "#3b82f6",
         background: "#f1f5f9",
       },
     },
   },
   plugins: [],
-}
-
+};
